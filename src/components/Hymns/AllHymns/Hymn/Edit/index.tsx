76886ad6b20b76c6a402f8df1555f8b9215a1272
ThomasBackens <thomas.backens@yahoo.com>
2021-04-01 16:37:06 -0500

    added the ability to edit a hymn

---
+++ src/components/Hymns/AllHymns/Hymn/Edit/index.tsx
@@ -0,0 +1,140 @@
+import React, { ReactElement } from "react";
+import { Formik, Field, Form } from "formik";
+import {
+  Box,
+  Button,
+  Modal,
+  Paper,
+  Backdrop,
+  Fade,
+  Typography,
+} from "@material-ui/core";
+import { TextField } from "formik-material-ui";
+import useStyles from "./styles";
+
+interface Values {
+  name: string;
+  number: number;
+}
+
+interface HymnValues {
+  name: string;
+  number: number;
+  logs: { logged: string; by: string }[];
+}
+
+interface EditModalProps {
+  open: boolean;
+  handleClose: () => void;
+  data: HymnValues;
+  editMutation: (values: Values) => void;
+}
+
+const EditModal: React.FC<EditModalProps> = ({
+  open,
+  handleClose,
+  data,
+  editMutation,
+}: EditModalProps): ReactElement => {
+  const s = useStyles();
+
+  const initialValues: {
+    name: string;
+    number: number;
+  } = {
+    name: data.name,
+    number: data.number,
+  };
+
+  return (
+    <Modal
+      className={s.modal}
+      BackdropComponent={Backdrop}
+      closeAfterTransition
+      BackdropProps={{
+        timeout: 500,
+      }}
+      onClose={handleClose}
+      open={open}
+    >
+      <Fade in={open}>
+        <Paper className={s.modalBackground}>
+          <Box padding={4} boxShadow={4}>
+            <Formik
+              initialValues={initialValues}
+              validate={(values) => {
+                const errors: Partial<{
+                  name: string;
+                  number: string;
+                }> = {};
+
+                if (!values.name) {
+                  errors.name = "Hymn Name is required";
+                }
+                if (!values.number) {
+                  errors.number = "Hymn Number is required";
+                }
+                return errors;
+              }}
+              onSubmit={(values: Values) => {
+                editMutation(values);
+                handleClose();
+              }}
+            >
+              {({ submitForm }) => (
+                <Form>
+                  <Box>
+                    <Typography className={s.title}>Edit Hymn</Typography>
+                  </Box>
+                  <Box marginTop={4}>
+                    <Field
+                      className={s.field}
+                      component={TextField}
+                      placeholder="Hymn name"
+                      type="text"
+                      name="name"
+                    />
+                  </Box>
+                  <Box marginTop={2}>
+                    <Field
+                      className={s.field}
+                      style={{ width: 242 }}
+                      component={TextField}
+                      placeholder="Hymn number"
+                      type="number"
+                      name="number"
+                    />
+                  </Box>
+                  <Box
+                    margin={1}
+                    marginTop={8}
+                    display="flex"
+                    justifyContent="flex-end"
+                  >
+                    <Button
+                      className={s.cancelButton}
+                      variant="contained"
+                      onClick={handleClose}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      className={s.saveHymnButton}
+                      variant="contained"
+                      color="primary"
+                      onClick={submitForm}
+                    >
+                      Save
+                    </Button>
+                  </Box>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+        </Paper>
+      </Fade>
+    </Modal>
+  );
+};
+
+export default EditModal;
--- src/components/Hymns/AllHymns/index.tsx
+++ src/components/Hymns/AllHymns/index.tsx
@@ -1,20 +1,62 @@
-import React, { ReactElement } from "react";
+import React, { useState, ReactElement } from "react";
 import { Box } from "@material-ui/core";
 import Hymn from "./Hymn";
 import hymnsList from "./hymnsList";
+import EditModal from "./Hymn/Edit";
+
+interface Values {
+  name: string;
+  number: number;
+}
+
+interface HymnValues {
+  name: string;
+  number: number;
+  logs: { logged: string; by: string }[];
+}
 
 const AllHymns = (): ReactElement => {
+  const [data, setData] = useState<HymnValues>({
+    name: "",
+    number: 0,
+    logs: [{ logged: "", by: "" }],
+  });
+  const [editModalOpen, setEditModalOpen] = useState<boolean>(false);
+
+  const openEditModal = (values: HymnValues) => {
+    setEditModalOpen(true);
+    setData(values);
+  };
+
+  const editHymn = (values: Values) => {
+    const selectedhymn = hymnsList.filter(
+      (hymn) => hymn.number === data.number
+    );
+
+    selectedhymn[0].name = values.name;
+    selectedhymn[0].number = values.number;
+  };
+
   return (
-    <Box display="flex" justifyContent="center" flexDirection="column">
-      {hymnsList.map((hymn) => (
-        <Hymn
-          name={hymn.name}
-          number={hymn.number}
-          logs={hymn.logs}
-          key={hymn.number}
-        />
-      ))}
-    </Box>
+    <>
+      <EditModal
+        open={editModalOpen}
+        handleClose={() => setEditModalOpen(false)}
+        data={data}
+        editMutation={editHymn}
+      />
+      <Box display="flex" justifyContent="center" flexDirection="column">
+        {hymnsList.map((hymn) => (
+          <Hymn
+            name={hymn.name}
+            number={hymn.number}
+            logs={hymn.logs}
+            handleEdit={() => openEditModal(hymn)}
+            key={hymn.number}
+          />
+        ))}
+      </Box>
+    </>
   );
 };
 
